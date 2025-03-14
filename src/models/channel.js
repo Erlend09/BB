c3005470c99f7d656b8a65a1785af0b15c865d56
Erlend09 <erlenda@uia.no>
2025-03-14 09:25:29 +0100

    Fullførte flere steg

--- src/models/channel.js
+++ src/models/channel.js
@@ -1,4 +1,4 @@
-const pool = require("../db");
+const pool = require("../../db");
 
 const Channel = {
     async create(name, groupId, type = "text") {
--- src/models/group.js
+++ src/models/group.js
@@ -1,4 +1,4 @@
-const pool = require("../db");
+const pool = require("../../db");
 
 const Group = {
     async create(name, ownerId) {
